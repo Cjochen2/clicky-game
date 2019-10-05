a875e5cdd805bde8cb964f02b95ba937de951ab8
Curtis Jochen <curtis.jochen@gmail.com>
2019-10-05 12:40:41 -0400

    Updates

---
+++ precache-manifest.0c4ca8b0d896c3aff8d0021d15f389d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3588c79072fe6fcbe7f9ea34c7b7906",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "079ee9660e354f9d3125",
+    "url": "/clicky-game/static/css/main.2feccd46.chunk.css"
+  },
+  {
+    "revision": "b45c15f4fc7578711934",
+    "url": "/clicky-game/static/js/2.229b0fad.chunk.js"
+  },
+  {
+    "revision": "079ee9660e354f9d3125",
+    "url": "/clicky-game/static/js/main.80b66d17.chunk.js"
+  },
+  {
+    "revision": "18967ef35086d38857e0",
+    "url": "/clicky-game/static/js/runtime-main.5319c75b.js"
+  }
+]);
