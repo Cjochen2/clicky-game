6fa541a0f439b1abb4c6511578dbcee955f9fb19
Curtis Jochen <curtis.jochen@gmail.com>
2020-01-29 13:11:30 -0500

    Added instructions to the clicky game web app

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./style.css";
 
 function Header(props) {
-  return <h1 className="title">{props.children}</h1>;
+  return (
+    <div className='heading'>
+      <h1 className="title">{props.children}</h1>
+      <h3 className='rules'>
+        The Rules of the clicky game are simple!
+    </h3>
+      <ol>
+        <li>Click a card</li>
+        <li>On Each click the deck will shuffle and you must not click the same card</li>
+        <li>After each successful attempt your score counter goes up if you lose the score counter will reset</li>
+      </ol>
+    </div>
+  )
 }
 
 export default Header;
